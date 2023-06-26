b6c09fcd4da5edc5282966590510c0aa40fa7563
sunshine <sunshineroprer@gmail.com>
2023-06-26 14:17:50 +0800

    update:左侧菜单显示问题

--- src/layout/aside/logo/index.vue
+++ src/layout/aside/logo/index.vue
@@ -2,9 +2,12 @@
   <div
     class="h-[60px] py-4"
   >
-    <div class="text-center mb-2 dark:text-white">
-      Vue-Sun-Admin
+    <div class="i-carbon-sun text-orange-400 text-4xl mx-auto cursor-pointer mb-2" />
+    <div
+      class="text-center mb-2 dark:text-white b"
+      style="font-family: Avenir,Helvetica Neue,Arial,Helvetica,sans-serif; color: #435ebe"
+    >
+      Vue <span class="text-orange-400">Sun</span> Admin
     </div>
-    <div class="i-carbon-sun text-orange-400 text-3xl mx-auto cursor-pointer" />
   </div>
 </template>
