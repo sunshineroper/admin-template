dc80557e755d7386306b1286d5d8a0e7d9bc24f7
sunshine <sunshineroprer@gmail.com>
2023-07-06 14:34:37 +0800

    update: 优化tags-tab

--- src/layout/header/tags-tab/index.vue
+++ src/layout/header/tags-tab/index.vue
@@ -9,9 +9,9 @@
     >
       <el-tab-pane
         v-for="item in histories"
-        :key="item.name"
-        :label="item.title"
-        :name="item.name"
+        :key="formatString(item)"
+        :label="fmtTile(item.meta.title, item)"
+        :name="formatString(item)"
       />
     </el-tabs>
   </el-scrollbar>
@@ -30,12 +30,32 @@ const histories = ref([])
 const cookies = useCookies(['locale'])
 
 const tagsTabHeight = ref('56px')
+
+const formatString = (router) => {
+  const { name, params, query } = router
+  return `${name}${JSON.stringify(params)}${JSON.stringify(query)}`
+}
+
+const fmtTile = (title, router) => {
+  const reg = /\$\{(.+?)\}/
+  const reg_g = /\$\{(.+?)\}/g
+  const result = title.match(reg_g)
+  if (result) {
+    result.forEach((item) => {
+      const key = item.match(reg)[1]
+      const value = router.params[key] || router.query[key]
+      title = title.replace(item, value)
+    })
+  }
+  return title
+}
+
 const initHistories = () => {
   if (histories.value.length === 0 && cookies.get('histories'))
     histories.value = cookies.get('histories')
   const currentTab = cookies.get('currentTab')
   if (currentTab)
-    currentTabName.value = currentTab.name
+    currentTabName.value = currentTab
 }
 
 onMounted(() => {
@@ -46,12 +66,12 @@ const changeRouter = (r) => {
   const { name, params, query } = r
 
   router.push({ name, params, query })
-  currentTabName.value = name
-  cookies.set('currentTab', r)
+  currentTabName.value = formatString(r)
+  cookies.set('currentTab', formatString(r))
 }
 
 const onHandleCloseClick = (name) => {
-  const idx = histories.value.findIndex(item => item.name === name)
+  const idx = histories.value.findIndex(item => formatString(item) === name)
   let currentTab
   if (idx !== -1) {
     if (idx === 0 && histories.value.length === 2)
@@ -74,9 +94,26 @@ function onTagsTabClick({ index }) {
     changeRouter(currentTab)
 }
 
+const routerSome = (r1, r2) => {
+  if (r1.name !== r2.name)
+    return false
+  if (Reflect.ownKeys(r1.query).length !== Reflect.ownKeys(r2.query).length)
+    return false
+  if (Reflect.ownKeys(r1.params).length !== Reflect.ownKeys(r2.params).length)
+    return false
+  for (const key in r1.query) {
+    if (r1.query[key] !== r2.query[key])
+      return false
+  }
+  for (const key in r1.params) {
+    if (r1.params[key] !== r2.params[key])
+      return false
+  }
+  return true
+}
 const setHistory = (router) => {
   const { path, params, name, query, meta } = router
-  const flag = histories.value.some(item => item.path === path)
+  const flag = histories.value.some(item => routerSome(item, router))
   if (!flag) {
     histories.value.push({
       name,
@@ -87,9 +124,9 @@ const setHistory = (router) => {
       meta,
     })
   }
-  currentTabName.value = name
+  currentTabName.value = formatString(router)
   cookies.set('histories', histories.value)
-  cookies.set('currentTab', router)
+  cookies.set('currentTab', formatString(router))
 }
 
 watch(() => route, (to, from) => {
